11af9ccec008df56b04f4ab21a454d0aa25021d1
Roman Senin <romansenin15@gmail.com>
2022-12-09 15:26:50 -0800

    solve: day 6 part 1

---
+++ day6/part1/index.js
@@ -0,0 +1,54 @@
+import { readFileSync } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const input = readFileSync(
+  path.join(path.dirname(__filename), "../input.txt"),
+  {
+    encoding: "utf8",
+  }
+);
+
+const findNumCharactersProcessed = function (input) {
+  const seenLetters = {};
+  let left = 0;
+  let right = 0;
+  let count = 0;
+  while (right < input.length) {
+    let letter = input[right];
+    if (letter in seenLetters) {
+      if (seenLetters[letter] === 0) {
+        count++;
+      } else if (seenLetters[letter] === 1) {
+        count--;
+      }
+      seenLetters[letter]++;
+    } else {
+      seenLetters[letter] = 1;
+      count++;
+    }
+
+    if (count === 4) {
+      return right + 1;
+    }
+
+    if (right - left === 3) {
+      letter = input[left];
+      if (letter in seenLetters) {
+        if (seenLetters[letter] === 1) {
+          count--;
+        } else if (seenLetters[letter] === 2) {
+          count++;
+        }
+        seenLetters[letter]--;
+      }
+      left++;
+    }
+    right++;
+  }
+};
+
+const answer = findNumCharactersProcessed(input);
+console.log("answer:", answer);
